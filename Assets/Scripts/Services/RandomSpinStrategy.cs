17d213d3ba36aefcdcc68a8e79ce227c57d37584
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-06-12 17:56:09 +0300

    random spin strategy class
    used to get index

---
+++ Assets/Scripts/Services/RandomSpinStrategy.cs
@@ -0,0 +1,13 @@
+using WheelOfFortune.Data;
+using Random = UnityEngine.Random;
+
+namespace WheelOfFortune.Services
+{
+    public class RandomSpinStrategy : IWheelSpinStrategy
+    {
+        public int GetWinningIndex(WheelConfigSO config)
+        {
+            return Random.Range(0, config.Slices.Length);
+        }
+    }
+}
