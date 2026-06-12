668f1ce061c8db6fb8c0cf1a7f6bd02a2e94789f
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-06-12 17:52:06 +0300

    reward service

---
+++ Assets/Scripts/Services/RewardService.cs
@@ -0,0 +1,36 @@
+using WheelOfFortune.Data;
+using WheelOfFortune.Domain;
+using WheelOfFortune.Events;
+using WheelOfFortune.Interfaces;
+
+namespace WheelOfFortune.Services
+{
+    public class RewardService : IRewardService
+    {
+        private readonly IEventBus _eventBus;
+        private readonly CollectedRewards _rewards;
+
+        public RewardService(IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+            _rewards = new CollectedRewards();
+        }
+
+        public void Collect(RewardItemSO item)
+        {
+            _rewards.Add(item);
+            _eventBus.Publish(new OnRewardCollected(_rewards.Clone()));
+        }
+
+        public void ClearAll()
+        {
+            _rewards.Clear();
+            _eventBus.Publish(new OnBombHit());
+        }
+
+        public CollectedRewards GetCurrentRewards()
+        {
+            return _rewards;
+        }
+    }
+}
