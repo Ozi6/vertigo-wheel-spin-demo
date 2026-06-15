149f3cdf49234073e16fe1edbd3bf6a582d3a835
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-06-15 19:57:34 +0300

    gridsizefitting utility class

---
+++ Assets/Scripts/Utility/GridContentSizer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Mathf = UnityEngine.Mathf;
+
+public class GridContentSizer : MonoBehaviour
+{
+    public GridLayoutGroup grid;
+    public RectTransform content;
+
+    void OnTransformChildrenChanged()
+    {
+        UpdateSize();
+    }
+
+    void UpdateSize()
+    {
+        int itemCount = content.childCount;
+
+        int columns = Mathf.FloorToInt(
+            (content.rect.width + grid.spacing.x) /
+            (grid.cellSize.x + grid.spacing.x)
+        );
+
+        columns = Mathf.Max(1, columns);
+
+        int rows = Mathf.CeilToInt((float)itemCount / columns);
+
+        float height =
+            rows * grid.cellSize.y +
+            (rows - 1) * grid.spacing.y +
+            grid.padding.top +
+            grid.padding.bottom;
+
+        content.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Vertical,
+            height
+        );
+    }
+}
